7cc0e822af4ef85afa2aaa6941a4b93b624c40bb
suprith14 <suprithcd14@gmail.com>
2025-05-19 21:43:57 +0530

    first commit111

--- src/config/db.js
+++ src/config/db.js
@@ -3,11 +3,14 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://admin:secret@192.168.31.230:27017/NodeBE") //connect to MongoDB database
-        console.log("MongoDB connected successfully")
+        await mongoose.connect("mongodb://admin:secret@192.168.31.230:27017/NodeBE?authSource=admin", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+});        console.log("MongoDB connected successfully")
     } catch (err) {
 
         console.log("MongoDB not connected ",err.message);
+    process.exit(1); //exit the process with error code 1
     }
 }
 
